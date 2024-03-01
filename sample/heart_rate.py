3c5d84e55e07960ef2a25dd7746a4ba77cc58efc
Lehi Chaves <lehi_chaves_93@hotmail.com>
2024-03-01 16:08:32 -0700

    CSE111

---
+++ sample/heart_rate.py
@@ -0,0 +1,11 @@
+# Python Review - Week 1
+
+age = int(input("How old are you? "))
+
+heart_rate = 220 - age
+heart_rate_min = heart_rate * 0.65
+heart_rate_max = heart_rate * 0.85
+
+print (f" When you physically exercise to strengthen your heart, you should \n \
+keep your heart rate between {heart_rate_min} and {heart_rate_max} beats per minute.")
+
